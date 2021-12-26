07716ed088858f542e1cdcaa4b6fd33ef91b7330
Sujith <sujith.kp@hotmail.com>
2021-12-27 04:06:20 +0530

    Added GradingStudent problem fro Hacker Rank.

---
+++ GeeksForGeeksProblems/HackerRank/GradingStudent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeeksForGeeksProblems.HackerRank
+{
+    public class GradingStudentSolution
+    {
+        public static List<int> GradingStudents(List<int> grades)
+        {
+            var result = new List<int>();
+
+            foreach (var num in grades)
+            {
+                if (num < 38)
+                {
+                    result.Add(num);
+                    continue;
+                }
+
+                if (5 - num % 5 < 3)
+                {
+                    result.Add(num + (5 - num % 5));
+                    continue;
+                }
+
+                result.Add(num);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProgrammingChallenges.Tests/GradingStudentTest.cs
@@ -0,0 +1,25 @@
+﻿using GeeksForGeeksProblems.HackerRank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProgrammingChallenges.Tests
+{
+    public class GradingStudentTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var a = new List<int> { 73, 67, 38, 33 };
+
+            var expectedResult = new List<int> { 75, 67, 40, 33 };
+
+            var result = GradingStudentSolution.GradingStudents(a);
+
+            Assert.Equal(result, expectedResult);
+        }
+    }
+}
