62eb524d66978e12cb4e585175aadbe9167316ac
Sujith <sujith.kp@hotmail.com>
2022-01-07 16:05:50 +0530

    Added Encryption

---
+++ GeeksForGeeksProblems/HackerRank/Encryption.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeeksForGeeksProblems.HackerRank
+{
+    public class Encryption
+    {
+        public static string encryption(string s)
+        {
+            s = s.Replace(" ", string.Empty);
+
+            double sqrt = Math.Sqrt(s.Length);
+
+            var rows = (int)Math.Ceiling(sqrt);
+            var cols = (int)Math.Ceiling(sqrt);
+
+            var matrix = new char[rows, cols];
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    if (((cols * r) + c) >= s.Length)
+                        matrix[r, c] = '\0';
+                    else
+                        matrix[r, c] = s[(cols * r) + c];
+                }
+            }
+            
+            StringBuilder strb = new StringBuilder();
+
+            for (int c = 0; c < cols; c++)
+            {
+                for (int r = 0; r < rows; r++)
+                {
+                    var ch = matrix[r, c];
+
+                    if (ch == '\0')
+                        break;
+
+                    strb.Append(ch);
+                }
+
+                strb.Append(" ");
+            }
+
+            return strb.ToString().Trim();
+        }
+    }
+}
---
+++ ProgrammingChallenges.Tests/EncryptionTests.cs
@@ -0,0 +1,21 @@
+﻿using GeeksForGeeksProblems.HackerRank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProgrammingChallenges.Tests
+{
+    public class EncryptionTests
+    {
+        [Theory]
+        [InlineData("if man was meant to stay on the ground god would have given us roots")]
+        [InlineData("chillout")]
+        public void Test1(string str)
+        {
+            var result = Encryption.encryption(str);
+        }
+    }
+}
