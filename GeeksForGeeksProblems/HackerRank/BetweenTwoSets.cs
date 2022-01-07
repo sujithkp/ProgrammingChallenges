1665f9b0c9597f1e1635a42f22acef435f93c4a2
Sujith <sujith.kp@hotmail.com>
2022-01-07 09:03:35 +0530

    Added Between two sets.

---
+++ GeeksForGeeksProblems/HackerRank/BetweenTwoSets.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeeksForGeeksProblems.HackerRank
+{
+    public class BetweenTwoSets
+    {
+        public static int getTotalX(List<int> a, List<int> b)
+        {
+            var max = b.Max() * b.Max();
+
+            var nums = new List<int>();
+
+            for (int i = a.Max(); i <= max; i++)
+            {
+                var alldivisible = true;
+
+                foreach (var ele in a)
+                    if (i % ele != 0)
+                    {
+                        alldivisible = false;
+                        break;
+                    }
+
+                if (alldivisible) nums.Add(i);
+            }
+
+            var result = new List<int>();
+
+            foreach (var element in nums)
+            {
+                var alldivisible = true;
+
+                foreach (var div in b)
+                    if (div % element != 0)
+                    {
+                        alldivisible = false;
+                        break;
+                    }
+                if (alldivisible)
+                    result.Add(element);
+            }
+
+            return result.Count;
+        }
+
+    }
+}
---
+++ ProgrammingChallenges.Tests/BetweenTwoSetsTests.cs
@@ -0,0 +1,31 @@
+﻿using GeeksForGeeksProblems.HackerRank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProgrammingChallenges.Tests
+{
+    public class BetweenTwoSetsTests
+    {
+        [Theory]
+        [InlineData()]
+        public void Test1()
+        {
+            var result = BetweenTwoSets.getTotalX(new List<int> { 3, 4 }, new List<int> { 24, 48 });
+
+            Assert.Equal(2, result);
+        }
+
+        [Theory]
+        [InlineData()]
+        public void Test2()
+        {
+            var result = BetweenTwoSets.getTotalX(new List<int> { 2, 4 }, new List<int> { 16, 32, 96 });
+
+            Assert.Equal(3, result);
+        }
+    }
+}
