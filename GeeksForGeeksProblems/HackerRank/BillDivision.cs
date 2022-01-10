acdd1e0c961b65297da38715787b32a03cb6068f
Sujith <sujith.kp@hotmail.com>
2022-01-10 18:45:16 +0530

    Added BillDivision

---
+++ GeeksForGeeksProblems/HackerRank/BillDivision.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeeksForGeeksProblems.HackerRank
+{
+    public class BillDivision
+    {
+        public static void bonAppetit(List<int> bill, int k, int b)
+        {
+            var total = bill.Sum() - bill[k];
+
+            var share = total / 2;
+
+            if (share == b)
+            {
+                Console.Write("Bon Appetit");
+                return;
+            }
+
+            Console.Write( b - share);
+        }
+    }
+}
