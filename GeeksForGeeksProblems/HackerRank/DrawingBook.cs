ed19de9243f943b087f0a40bfff6fdb6dc2720e2
Sujith <sujith.kp@hotmail.com>
2022-01-15 10:22:44 +0530

    Added Drawingbook problem.

---
+++ GeeksForGeeksProblems/HackerRank/DrawingBook.cs
@@ -0,0 +1,18 @@
+﻿namespace GeeksForGeeksProblems.HackerRank
+{
+    public class DrawingBook
+    {
+        public static int pageCount(int n, int p)
+        {
+            var fromStart = p / 2;
+            n = n + (n % 2 == 0 ? 1 : 0);
+
+            var fromEnd = (n - p) / 2;
+
+            if (fromStart < fromEnd)
+                return fromStart;
+
+            return fromEnd;
+        }
+    }
+}
---
+++ ProgrammingChallenges.Tests/DrawingBookTests.cs
@@ -0,0 +1,19 @@
+﻿using GeeksForGeeksProblems.HackerRank;
+using Xunit;
+
+namespace ProgrammingChallenges.Tests
+{
+    public class DrawingBookTests
+    {
+        [Theory]
+        [InlineData(5, 4, 0)]
+        [InlineData(6, 2, 1)]
+        [InlineData(6, 5, 1)]
+        public void Test1(int totalPages, int requiredPage, int expectedResult)
+        {
+            var result = DrawingBook.pageCount(totalPages, requiredPage);
+
+            Assert.Equal(result, expectedResult);
+        }
+    }
+}
