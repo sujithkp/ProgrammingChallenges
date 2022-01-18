42765d56d86df1a9115eb933439a6aac6af7a2e7
Sujith <sujith.kp@hotmail.com>
2022-01-18 10:23:56 +0530

    Added RepeatString

---
+++ GeeksForGeeksProblems/HackerRank/RepeatedString.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace GeeksForGeeksProblems.HackerRank
+{
+    public class RepeatedString
+    {
+        public static long repeatedString(string s, long n)
+        {
+            var numberOfas = s.Count(x => x == 'a');
+            var repeatetionRequired = n / s.Length;
+            var partsrequired = n % s.Length;
+            var asinparts = s.Take((int)partsrequired).Count(x => x == 'a');
+
+            var totalas = numberOfas * repeatetionRequired + asinparts;
+
+            return totalas;
+        }
+
+    }
+}
---
+++ ProgrammingChallenges.Tests/RepeatedStringTests.cs
@@ -0,0 +1,19 @@
+﻿using GeeksForGeeksProblems.HackerRank;
+using Xunit;
+
+namespace ProgrammingChallenges.Tests
+{
+    public class RepeatedStringTests
+    {
+        [Theory]
+        [InlineData("abcac", 10, 4)]
+        [InlineData("aba", 10, 7)]
+        [InlineData("a", 1000000000000, 1000000000000)]
+        public void Test3(string input, long n, long expected)
+        {
+            var result = RepeatedString.repeatedString(input, n);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
