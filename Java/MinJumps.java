1a6a1fe6e5c1249593d91634e16e0a439f36190f
Sujith <sujith.kp@hotmail.com>
2022-09-18 08:47:58 +0530

    Attempt to solve MinJumps

--- Java/MinJumps.java
+++ Java/MinJumps.java
@@ -1,8 +1,12 @@
+import java.io.Console;
+
 public class MinJumps {
     public static void main (String[] args) {
         //int  c =  minJumps(new int[] { 1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9} );
-       // int c =  minJumps(new int[] { 1, 4, 3, 2, 6, 7} );
+        //int c =  minJumps(new int[] { 1, 4, 3, 2, 6, 7} );
         int c =  minJumps(new int[] { 2, 3, 1, 1, 2, 4, 2, 0, 1, 1} );
+        //int c =  minJumps(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1} );
+        
 
         System.out.println(c);
     }
@@ -11,27 +15,21 @@ public class MinJumps {
 
         int currIndex = 0;
         int jumps = 0;
-        int nextIndex = 0;
+
+        int nextjumpLength = arr[0];
 
         if (arr.length == 0) return -1;
 
-        while (true) {
-
-            currIndex = nextIndex;
-
-            int elementAtCurrIndex = arr[currIndex];
-
-            nextIndex = currIndex + elementAtCurrIndex;
-
-            if (nextIndex >= arr.length -1  )
-            {
+        while (currIndex < arr.length - 1) {
+            if(currIndex + nextjumpLength >= arr.length - 1) {
                 jumps++;
                 break;
             }
 
-            jumps ++;
+            currIndex += nextjumpLength;
+            nextjumpLength = arr[currIndex];
+            jumps++;
         }
-
         return jumps;
     }
 }
