c3e6e6fb6403a72597ed4055a8f0cf21a04d2c6b
Sujith <sujith.kp@hotmail.com>
2022-10-21 16:24:38 +0530

    Added LinkedListPalindrome.

---
+++ GeeksForGeeksProblems/LeetCode/LinkedlistPalindrome.cs
@@ -0,0 +1,74 @@
+﻿//https://leetcode.com/problems/palindrome-linked-list/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeeksForGeeksProblems.LeetCode
+{
+
+    public class LinkedList<T>
+    {
+        public ListNode Head { get; private set; }
+
+        public LinkedList(IList<int> elements)
+        {
+            Init(elements);
+        }
+
+        private void Init(IList<int> elements)
+        {
+            if (elements == null || elements.Count == 0)
+                return;
+
+            var prev = this.Head = new ListNode(elements[0]);
+
+            for (int i = 1; i < elements.Count; i++)
+                prev.next = new ListNode(elements[i]);
+        }
+
+    }
+
+    public class ListNode
+    {
+        public int val;
+
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class LinkedlistPalindrome
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            var stack = new Stack<int>();
+
+            var curr = head;
+
+            while (curr != null)
+            {
+                stack.Push(curr.val);
+                curr = curr.next;
+            }
+
+            curr = head;
+
+            while (curr != null)
+            {
+                if (curr.val != stack.Pop())
+                    return false;
+
+                curr = curr.next;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ProgrammingChallenges.Tests/LeetCodeTests/LinkedListPalindromeTests.cs
@@ -0,0 +1,44 @@
+﻿using GeeksForGeeksProblems.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProgrammingChallenges.Tests.LeetCodeTests
+{
+    public class LinkedListPalindromeTests
+    {
+        [Theory]
+        [InlineData("1 2 2 1")]
+        public void IsPalindromeTest(string arr)
+        {
+            var nums = arr.Split(' ').Select(x => int.Parse(x)).ToList();
+
+            var llist = new GeeksForGeeksProblems.LeetCode.LinkedList<int>(nums);
+
+            var linkedListPalindrome = new LinkedlistPalindrome();
+
+            var result = linkedListPalindrome.IsPalindrome(llist.Head);
+
+            Assert.Equal(result, true);
+        }
+
+
+        [Theory]
+        [InlineData("1 2 2")]
+        public void IsPalindromeTest2(string arr)
+        {
+            var nums = arr.Split(' ').Select(x => int.Parse(x)).ToList();
+
+            var llist = new GeeksForGeeksProblems.LeetCode.LinkedList<int>(nums);
+
+            var linkedListPalindrome = new LinkedlistPalindrome();
+
+            var result = linkedListPalindrome.IsPalindrome(llist.Head);
+
+            Assert.Equal(result, false);
+        }
+    }
+}
