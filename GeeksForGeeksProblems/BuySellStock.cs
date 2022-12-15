e88737cbaa6b6dd9c9534b0ddb6b803faf7ab4d0
Sujith <sujith.kp@hotmail.com>
2022-12-16 00:30:24 +0530

    max profit from Stock buy sell

---
+++ GeeksForGeeksProblems/BuySellStock.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeeksForGeeksProblems
+{
+    public class BuySellStock
+    {
+        public static List<List<int>> MaxProfitFromstockBuySell(int[] prices)
+        {
+            var sharePair = new List<List<int>>();
+
+            int smallestPrice = prices[0];
+            int largestPrice = prices[0];
+            int maxProfit = 0;
+
+            var maxprofits = new List<int>();
+
+            var smallestPriceIndex = 0;
+            int largestPriceIndex = 0;
+
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                int profit = prices[i] - smallestPrice;
+
+                if (prices[i] <= smallestPrice)
+                {
+                    if (maxProfit > 0)
+                    {
+                        sharePair.Add(new List<int>() { smallestPriceIndex, largestPriceIndex });
+                        maxprofits.Add(maxProfit);
+                    }
+
+                    largestPriceIndex = smallestPriceIndex = i;
+                    largestPrice = smallestPrice = prices[i];
+                    maxProfit = 0;
+                }
+
+                if (prices[i] > largestPrice)
+                {
+                    largestPrice = prices[i];
+
+                    maxProfit = largestPrice - smallestPrice;
+
+                    largestPriceIndex = i;
+                }
+            }
+
+            if (maxProfit > 0)
+            {
+                maxprofits.Add(maxProfit);
+                sharePair.Add(new List<int> { smallestPriceIndex, largestPriceIndex });
+            }
+
+            return sharePair;
+        }
+    }
+}
--- GeeksForGeeksProblems/Program.cs
+++ GeeksForGeeksProblems/Program.cs
@@ -19,6 +19,15 @@ namespace GeeksForGeeksProblems
 
         public static void Main(string[] args)
         {
+            BuySellStock.stockBuySell(new int[] { 822, 754, 1689, 305, 214, 782, 1463, 1432, 1382, 1734,
+    948, 231, 210, 1676, 877, 670, 1384, 725, 1370, 412, 724, 371, 928, 358, 533, 1031, 850,
+    1555, 1064, 845, 1692, 514, 630, 1333, 1640, 315, 1639, 1792, 1779, 1325, 1619, 1400,
+    378, 145, 913, 901, 1652, 530, 1259, 880, 303, 685, 1586 }, 53);
+
+            BuySellStock.stockBuySell(new int[] { 4, 2, 2, 2, 4 }, 5);
+
+            BuySellStock.stockBuySell(new int[] { 100, 180, 260, 310, 40, 535, 695 }, 7);
+
             SomeBulkyTask(5);
 
             Console.WriteLine("over");
@@ -115,7 +124,7 @@ namespace GeeksForGeeksProblems
 
     public interface SomeInterface
     {
-      //  public int SomeField; // not possible
+        //  public int SomeField; // not possible
     }
 
     public class Ingredient
